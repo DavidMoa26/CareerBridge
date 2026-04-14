e0b9226cf8ad9577e310860ea5a79b4bfa17d917
David Moalem <davidmoa26@gmail.com>
2026-04-14 12:30:44 +0300

    fix: add dummy env vars to CI build step to satisfy SDK constructors

--- src/services/uploadthing/client.ts
+++ src/services/uploadthing/client.ts
@@ -1,4 +1,4 @@
 import { env } from "@/data/env/server"
 import { UTApi } from "uploadthing/server"
 
-export const uploadthing = new UTApi({ token: env.UPLOADTHING_TOKEN })
+export const uploadthing = new UTApi({ token: env.UPLOADTHING_TOKEN ?? "placeholder_for_build" })
