7e68725e099d6b94c6b968a694a42a509369cbfc
David Moalem <davidmoa26@gmail.com>
2026-04-14 14:42:24 +0300

    fix: convert jest.config to .js to avoid ts-node requirement

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest.js")
+
+const createJestConfig = nextJest({
+  // Points to your Next.js app root so next/jest can load next.config.ts
+  dir: "./",
+})
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "node",
+}
+
+module.exports = createJestConfig(config)
